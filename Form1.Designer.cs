6575d12ab53a8f120ec8cb016203f4b744968c44
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2020-04-27 19:20:37 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DetectLaserApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPictureBox = new System.Windows.Forms.PictureBox();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.BtnDraw = new System.Windows.Forms.Button();
+            this.saveImageBTN = new System.Windows.Forms.Button();
+            this.DrawGroupBox = new System.Windows.Forms.GroupBox();
+            this.colorBox = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.setEraser = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.penThickness = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openImgBTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DrawPictureBox)).BeginInit();
+            this.DrawGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.penThickness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawPictureBox
+            // 
+            this.DrawPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.DrawPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.DrawPictureBox.Name = "DrawPictureBox";
+            this.DrawPictureBox.Size = new System.Drawing.Size(640, 480);
+            this.DrawPictureBox.TabIndex = 0;
+            this.DrawPictureBox.TabStop = false;
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.DisplayMember = "Red";
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue"});
+            this.comboBoxColor.Location = new System.Drawing.Point(717, 46);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxColor.TabIndex = 1;
+            this.comboBoxColor.Text = "Red";
+            this.comboBoxColor.SelectedIndexChanged += new System.EventHandler(this.comboBoxColor_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(788, 439);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 37);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Stop";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // BtnDraw
+            // 
+            this.BtnDraw.BackColor = System.Drawing.Color.Transparent;
+            this.BtnDraw.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDraw.ForeColor = System.Drawing.Color.Maroon;
+            this.BtnDraw.Location = new System.Drawing.Point(705, 82);
+            this.BtnDraw.Name = "BtnDraw";
+            this.BtnDraw.Size = new System.Drawing.Size(151, 28);
+            this.BtnDraw.TabIndex = 8;
+            this.BtnDraw.Text = "Start Drawing";
+            this.BtnDraw.UseVisualStyleBackColor = false;
+            this.BtnDraw.Click += new System.EventHandler(this.BtnDraw_Click);
+            // 
+            // saveImageBTN
+            // 
+            this.saveImageBTN.BackColor = System.Drawing.Color.Azure;
+            this.saveImageBTN.Location = new System.Drawing.Point(93, 192);
+            this.saveImageBTN.Name = "saveImageBTN";
+            this.saveImageBTN.Size = new System.Drawing.Size(106, 23);
+            this.saveImageBTN.TabIndex = 9;
+            this.saveImageBTN.Text = "Save drawing";
+            this.saveImageBTN.UseVisualStyleBackColor = false;
+            this.saveImageBTN.Click += new System.EventHandler(this.saveImageBTN_Click);
+            // 
+            // DrawGroupBox
+            // 
+            this.DrawGroupBox.Controls.Add(this.label1);
+            this.DrawGroupBox.Controls.Add(this.penThickness);
+            this.DrawGroupBox.Controls.Add(this.colorBox);
+            this.DrawGroupBox.Controls.Add(this.label3);
+            this.DrawGroupBox.Controls.Add(this.setEraser);
+            this.DrawGroupBox.Controls.Add(this.label2);
+            this.DrawGroupBox.Controls.Add(this.saveImageBTN);
+            this.DrawGroupBox.Location = new System.Drawing.Point(674, 140);
+            this.DrawGroupBox.Name = "DrawGroupBox";
+            this.DrawGroupBox.Size = new System.Drawing.Size(205, 225);
+            this.DrawGroupBox.TabIndex = 10;
+            this.DrawGroupBox.TabStop = false;
+            this.DrawGroupBox.Visible = false;
+            // 
+            // colorBox
+            // 
+            this.colorBox.BackColor = System.Drawing.Color.Black;
+            this.colorBox.Location = new System.Drawing.Point(125, 53);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(57, 37);
+            this.colorBox.TabIndex = 17;
+            this.colorBox.TabStop = false;
+            this.colorBox.Click += new System.EventHandler(this.colorBox_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Click to change";
+            // 
+            // setEraser
+            // 
+            this.setEraser.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.setEraser.Location = new System.Drawing.Point(54, 60);
+            this.setEraser.Name = "setEraser";
+            this.setEraser.Size = new System.Drawing.Size(49, 30);
+            this.setEraser.TabIndex = 15;
+            this.setEraser.Text = "Eraser";
+            this.setEraser.UseVisualStyleBackColor = false;
+            this.setEraser.Click += new System.EventHandler(this.setEraser_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Small", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 23);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Drawing color :";
+            // 
+            // penThickness
+            // 
+            this.penThickness.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.penThickness.Location = new System.Drawing.Point(24, 141);
+            this.penThickness.Maximum = 5;
+            this.penThickness.Minimum = 1;
+            this.penThickness.Name = "penThickness";
+            this.penThickness.Size = new System.Drawing.Size(140, 45);
+            this.penThickness.TabIndex = 18;
+            this.penThickness.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.penThickness.Value = 1;
+            this.penThickness.Scroll += new System.EventHandler(this.penThickness_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 18);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Brush thickness:";
+            // 
+            // openImgBTN
+            // 
+            this.openImgBTN.Location = new System.Drawing.Point(674, 439);
+            this.openImgBTN.Name = "openImgBTN";
+            this.openImgBTN.Size = new System.Drawing.Size(103, 37);
+            this.openImgBTN.TabIndex = 11;
+            this.openImgBTN.Text = "Open an Image";
+            this.openImgBTN.UseVisualStyleBackColor = true;
+            this.openImgBTN.Click += new System.EventHandler(this.openImgBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(902, 488);
+            this.Controls.Add(this.openImgBTN);
+            this.Controls.Add(this.DrawGroupBox);
+            this.Controls.Add(this.BtnDraw);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBoxColor);
+            this.Controls.Add(this.DrawPictureBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.DrawPictureBox)).EndInit();
+            this.DrawGroupBox.ResumeLayout(false);
+            this.DrawGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.colorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.penThickness)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DrawPictureBox;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button BtnDraw;
+        private System.Windows.Forms.Button saveImageBTN;
+        private System.Windows.Forms.GroupBox DrawGroupBox;
+        private System.Windows.Forms.PictureBox colorBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button setEraser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TrackBar penThickness;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openImgBTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,291 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace DetectLaserApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new VideoCapture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                   // "Start" for resuming capture
+                    btnStart.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+                captureInProgress = !captureInProgress;
+            }
+
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        private VideoCapture capture;  //takes images from camera as image frames
+        private bool captureInProgress;
+
+        int[] red = { 140, 140, 255 };
+        int[] green = { 0, 90, 100, 255, 0, 90 };
+        int[] blue = { 90, 255, 0, 80, 0, 80 };
+        int[] colorToDetect = new int[6];
+        bool drawing = false;
+        Point prevPoint = new Point();
+        Point currentPoint = new Point();
+        Bgr currentColor;
+        String currentCol;
+        double threshold = 25;
+        Bitmap drawBox = new Bitmap(640, 480);
+        Graphics g;
+        Point origin = new Point(0, 0);
+
+        Pen pen = new Pen(Color.Black, 1);
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Mat mat = capture.QueryFrame();
+            Image<Bgr, Byte> ImageFrame = mat.ToImage<Bgr, Byte>();
+            DetectPix(ImageFrame);
+        }
+
+        private void comboBoxColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentCol = comboBoxColor.Text;
+
+            if (currentCol == "Blue") colorToDetect = blue;
+            else if (currentCol == "Green") colorToDetect = green;
+            else colorToDetect = red;
+        }
+
+        public void DetectPix(Image<Bgr, byte> imageFrame)
+        {
+            int wdh = imageFrame.Width; //640
+            int hgt = imageFrame.Height; //480
+
+            float avgX = 0;
+            float avgY = 0;
+
+            int count = 0;
+            for (int i = 1; i < (wdh - 4); i = i + 3)
+            {
+                for (int j = 1; j < (hgt - 4); j = j + 3)
+                {
+                    currentColor = imageFrame[j, i];
+
+                    //due to the bad quality of the camera, red lazer will look as an extremly white dot(as it it extremly light) with a light pink circle arround them, 
+                    //so during the night i use the light pink as the detected color and during the day i will use the white dot.
+                    //int red = 255;
+                    //int blue = 140;
+                    //int green = 140; //light pink
+                    int red = 255;
+                    int blue = 255;
+                    int green = 255;  //white
+
+                    double dist = Math.Sqrt(Math.Pow(red - currentColor.Red, 2) + Math.Pow(green - currentColor.Green, 2) + Math.Pow(blue - currentColor.Blue, 2));
+
+                    if (dist < threshold)
+                    {
+                        avgX += i;
+                        avgY += j;
+                        count++;
+                    }
+                }
+            }
+
+            if (count > 0)
+            {
+                avgX = avgX / count;
+                avgY = avgY / count;
+                currentPoint.X = (int)avgX;
+                currentPoint.Y = (int)avgY;
+
+            }
+
+            if (!drawing)
+            {
+                if (count > 0)
+                {
+                    SetCursorPos(currentPoint.X + Location.X, currentPoint.Y + Location.Y);
+                }
+
+            }
+            else
+            {
+                if (count == 0)
+                {
+                    prevPoint = origin;
+                }
+                if (count > 0)
+                {
+
+
+                    if (prevPoint == origin)
+                    {
+                        prevPoint = currentPoint;
+                    }
+
+                    g.DrawLine(pen, prevPoint, currentPoint);
+                    prevPoint = currentPoint;
+                    //DrawPictureBox.Image = drawBox;
+                }
+            }
+            DrawPictureBox.Image = drawBox;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //setting the detected color to red by default
+            colorToDetect = red;
+
+            g = Graphics.FromImage(drawBox);
+        }
+
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+            if (!drawing)
+            {
+                DrawGroupBox.Visible = true;
+                drawing = true;
+                g.Flush();
+                g.Clear(Color.White);
+                //g = Graphics.FromImage(drawBox);
+                BtnDraw.Text = "Stop Drawing";
+            }
+            else
+            {
+                DrawGroupBox.Visible = false;
+                drawing = false;
+                g.Flush();
+                g.Clear(Color.White);
+                BtnDraw.Text = "Start Drawing";
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                if (!drawing)
+                {
+                    drawing = true;
+                    g.Flush();
+                    g.Clear(Color.White);
+                }
+                else
+                {
+                    drawing = false;
+                    g.Flush();
+                    g.Clear(Color.White);
+
+                }
+            }
+        }
+
+        private void saveImageBTN_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        this.DrawPictureBox.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        this.DrawPictureBox.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        this.DrawPictureBox.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
+
+
+        private void colorBox_Click(object sender, EventArgs e)
+        {
+
+            colorDialog1.ShowDialog();
+            pen.Color = colorDialog1.Color;
+            colorBox.BackColor = colorDialog1.Color;
+
+
+        }
+
+        private void setEraser_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.White;
+            colorBox.BackColor = Color.White;
+        }
+
+        private void penThickness_Scroll(object sender, EventArgs e)
+        {
+            pen.Width = penThickness.Value;
+        }
+
+        private void openImgBTN_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Open Image";
+                openFileDialog.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap btm = new Bitmap(openFileDialog.FileName);
+                    g.DrawImage(btm, origin);
+                }
+            }
+        }
+    }
+}
